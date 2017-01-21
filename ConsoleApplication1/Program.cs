2d50ebea57a3891b6ba18e15b206d9ef2608956f
zhanelderzach <miras_13@bk.ru>
2017-01-21 17:20:18 +0600

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static bool IsPrime(string x)
+        {
+            int a = int.Parse(x);
+
+            int cnt = 0; 
+
+            for (int i=1; i<=a; ++i)
+            {
+                if(a%i==0)
+                {
+                    cnt++;
+                }
+            }
+
+            if (cnt == 2) return true;
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i=0; i<args.Length; ++i)
+            {
+                if (IsPrime(args[i]))
+                {
+                    Console.WriteLine(args[i]);
+                }
+            }
+        }
+    }
+}
